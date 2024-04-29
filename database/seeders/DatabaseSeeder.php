2396dca8ac17f8575712c7bcab86cb2361748944
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-04-29 20:15:28 +0200

    initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            "usertype" => "ADMIN",
+            "email" => "admin@localhost.com",
+            "password" => Hash::make("12345678")
+        ]);
+    }
+}
